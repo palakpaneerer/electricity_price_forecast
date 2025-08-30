5992df4b3126b97de763bc0b5691137c9ebd0f1f
palakpaneerer <dinho.mako@gmail.com>
2025-08-30 21:48:07 +0100

    modify params page path

--- myproject/myproject/hyper_params.py
+++ myproject/myproject/hyper_params.py
@@ -3,12 +3,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import os
 
 # 02 Page Title
 st.title('ハイパーパラメーター')
 
 # 03 Load
-df_params_t = pd.read_parquet("models/artifacts/LightGBM_params.parquet")
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+df_params_t_path = os.path.join(APP_DIR, 'models', 'artifacts', 'LightGBM_params.parquet')
+df_params_t = pd.read_parquet(df_params_t_path, engine='pyarrow')
 
 # 04 Show 
 st.dataframe(df_params_t, use_container_width=True)
